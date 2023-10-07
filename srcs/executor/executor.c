8d579d7f78dccb23923d9e3c9309f540685ee4cc
anthony <anthonydev03@gmail.com>
2023-10-07 11:57:34 +0200

    executeru FILE fonction

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -9,22 +9,43 @@ void setup_redirection(int oldfd, int newfd, int *saved) {
   close(newfd);
 }
 
+#define TMP_FILENAME "/tmp/my_temp_file"
+
 FILE *setup_append_redirection(char *delimiter) {
-  char buffer[1024];
-  FILE *tempfile = tmpfile();
-  if (!tempfile) {
-    perror("tmpfile");
+  char *buffer;
+  int tempfile_fd;
+
+  // Création du fichier temporaire avec open
+  tempfile_fd =
+      open(TMP_FILENAME, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  if (tempfile_fd == -1) {
+    perror("open");
     return NULL;
   }
-  while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+
+  while ((buffer = readline("")) != NULL) {
     if (strncmp(buffer, delimiter, strlen(delimiter)) == 0 &&
         buffer[strlen(delimiter)] == '\n') {
+      free(buffer);
       break;
     }
-    fputs(buffer, tempfile);
+    // Écriture dans le fichier temporaire
+    write(tempfile_fd, buffer, strlen(buffer));
+    write(tempfile_fd, "\n",
+          1); // Ajouter le caractère de nouvelle ligne manuellement
+    free(buffer);
   }
-  rewind(tempfile);
-  return tempfile;
+
+  // Utilisation de lseek pour rembobiner le fichier
+  lseek(tempfile_fd, 0, SEEK_SET);
+
+  // Astuce pour obtenir un FILE * depuis le descripteur de fichier
+  dup2(tempfile_fd,
+       STDIN_FILENO); // Duplique le descripteur de fichier vers stdin
+  close(tempfile_fd); // Ferme le descripteur de fichier original
+
+  return stdin; // Retourne le flux stdin, qui est maintenant associé au fichier
+                // temporaire
 }
 
 void cleanup_append_redirection(int saved_stdin, FILE *tempfile) {
@@ -33,22 +54,22 @@ void cleanup_append_redirection(int saved_stdin, FILE *tempfile) {
 }
 
 void execute(t_node *root) {
-    if (!root)
-        return;
+  if (!root)
+    return;
 
-    if (root->type == NODE_COMMAND) {
-        execute_command(root);
-    } else if (root->type == NODE_PIPE) {
-        execute_pipeline(root);
-    } else if (root->type == NODE_REDIRECT_IN) {
-        execute_redirect_in(root);
-    } else if (root->type == NODE_REDIRECT_OUT) {
-        execute_redirect_out(root);
-    } else if (root->type == NODE_REDIRECT_IN_APPEND) {
-        execute_redirect_in_append(root);
-    } else if (root->type == NODE_REDIRECT_OUT_APPEND) {
-        execute_redirect_out_append(root);
-    } else {
-        write(2, "Error: Unknown node type\n", 23);
-    }
+  if (root->type == NODE_COMMAND) {
+    execute_command(root);
+  } else if (root->type == NODE_PIPE) {
+    execute_pipeline(root);
+  } else if (root->type == NODE_REDIRECT_IN) {
+    execute_redirect_in(root);
+  } else if (root->type == NODE_REDIRECT_OUT) {
+    execute_redirect_out(root);
+  } else if (root->type == NODE_REDIRECT_IN_APPEND) {
+    execute_redirect_in_append(root);
+  } else if (root->type == NODE_REDIRECT_OUT_APPEND) {
+    execute_redirect_out_append(root);
+  } else {
+    write(2, "Error: Unknown node type\n", 23);
+  }
 }
